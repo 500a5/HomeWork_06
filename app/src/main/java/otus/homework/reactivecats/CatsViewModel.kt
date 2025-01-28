c726e617f4d82cbdf7bf79af0182f4358c19af2f
500a5 <500a5@mail.ru>
2025-01-28 10:42:07 +0300

    1. Переведите сетевой запрос с `retrofit.Call` на RX цепочку. Для этого подключите Retrofit адаптер, поменяйте возвращаемые типы функций
    2. Поменяйте логику в `CatsViewModel.kt` с колбеков на RX. Логику обработки успеха/ошибки из коллбека необходимо перенести в терминальные коллбеки RX цепочки. Не забудьте очистить подписки когда `ViewModel` уничтожается
    
    3. Реализуйте функцию `otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFact`, так, чтобы она возвращала `Fact` со случайной строкой  из массива строк `R.array.local_cat_facts` обернутую в подходящий стрим(`Flowable`/`Single`/`Observable` и т.п)
    
    4. Реализуйте функцию `otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFactPeriodically` так, чтобы она эмитила `Fact` со случайной строкой из массива строк `R.array.local_cat_facts` каждые 2000 миллисекунд. Если вновь заэмиченный Fact совпадает с предыдущим - пропускаем элемент.
    
    5. Реализуйте функцию `otus.homework.reactivecats.CatsViewModel#getFacts` следующим образом:  каждые 2 секунды идем в сеть за новым фактом, если сетевой запрос завершился неуспешно, то в качестве фоллбека идем за фактом в уже реализованный `otus.homework.reactivecats.LocalCatFactsGenerator#generateCatFact`.

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -5,40 +5,69 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class CatsViewModel(
-    catsService: CatsService,
-    localCatFactsGenerator: LocalCatFactsGenerator,
-    context: Context
+    private val catsService: CatsService,
+    private val localCatFactsGenerator: LocalCatFactsGenerator,
+    private val context: Context
 ) : ViewModel() {
 
     private val _catsLiveData = MutableLiveData<Result>()
     val catsLiveData: LiveData<Result> = _catsLiveData
+    private val disposable = CompositeDisposable()
 
-    init {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsLiveData.value = Success(response.body()!!)
-                } else {
-                    _catsLiveData.value = Error(
-                        response.errorBody()?.string() ?: context.getString(
-                            R.string.default_error_text
-                        )
-                    )
-                }
-            }
+    /*init {
+        disposable.add(
+            catsService.getCatFact()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { fact -> _catsLiveData.value = Success(fact) },
+                    { error ->
+                        _catsLiveData.value =
+                            Error(error.message ?: context.getString(R.string.default_error_text))
+                    }
+                )
+        )
+    }
+*/
 
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                _catsLiveData.value = ServerError
-            }
-        })
+    override fun onCleared() {
+        super.onCleared()
+        catsService
+        disposable.clear()
     }
 
-    fun getFacts() {}
+    fun getFacts() {
+        disposable.add(
+            Observable.interval(2000, TimeUnit.MILLISECONDS)
+                .flatMapSingle {
+                    catsService.getCatFact()
+                        .subscribeOn(Schedulers.io())
+                        .map<Result> { fact -> Success(fact) }
+                        .onErrorResumeNext {
+                            localCatFactsGenerator.generateCatFact()
+                                .map<Result> { fact -> Success(fact) }
+                        }
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                    { result ->
+                        _catsLiveData.value = result
+                    },
+                    { error ->
+                        _catsLiveData.value = Error(
+                            error.message ?: context.getString(R.string.default_error_text)
+                        )
+                    }
+                )
+        )
+    }
 }
 
 class CatsViewModelFactory(
